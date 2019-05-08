9d9a89dc0268be2aa19c65b3f1524efaeef57bc7
Tyler Caceres <tyler.caceres@gmail.com>
2019-05-08 13:57:23 +0000

    creation of loopy-lighthouse.js prints 100 to 200 with conditions

---
+++ loopy-lighthouse.js
@@ -0,0 +1,26 @@
+function loopyLighthouse(){
+  var start = 100;
+  var end = 200;
+
+  for(var i = start; i <= end; i++){
+    if(i % 3 === 0 && i % 4 === 0){
+      console.log("LoopyLighthouse");
+    }else if(i % 3 === 0){
+      console.log("Loopy");
+    }else if(i % 4 === 0){
+      console.log("Lighthouse");
+    }else{
+      console.log(i);
+    }
+
+  }
+}
+
+loopyLighthouse();
+
+// Write a program that prints the numbers from 100 to 200 to the console, with three exceptions:
+
+// If the number is a multiple of 3, print the string "Loopy" instead of the number.
+// If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
+// If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
+// By print, we are of course referring to console.log.
