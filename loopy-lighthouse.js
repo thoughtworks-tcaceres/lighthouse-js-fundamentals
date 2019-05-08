f6f3f554dfda29f986ba6aa63349f1295ca0b8b8
Tyler Caceres <tyler.caceres@gmail.com>
2019-05-08 14:20:49 +0000

    updated version of loopy-lighthouse using 3 parameters

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,14 +1,15 @@
-function loopyLighthouse(){
-  var start = 100;
-  var end = 200;
+function loopyLighthouse(range, multiples, words){
+  //range should be an array of 2 numbers representing the start and end values for the loop
+  //multiples should be an array of 2 numbers representing the multiples you want to replace with words
+  //words should be an array of 2 strings representing the words that will replace the multiples.
 
-  for(var i = start; i <= end; i++){
-    if(i % 3 === 0 && i % 4 === 0){
-      console.log("LoopyLighthouse");
-    }else if(i % 3 === 0){
-      console.log("Loopy");
-    }else if(i % 4 === 0){
-      console.log("Lighthouse");
+  for(var i = range[0]; i <= range[1]; i++){
+    if(i % multiples[0] === 0 && i % multiples[1] === 0){
+      console.log(words[0] + words[1]);
+    }else if(i % multiples[0] === 0){
+      console.log(words[0]);
+    }else if(i % multiples[1] === 0){
+      console.log(words[1]);
     }else{
       console.log(i);
     }
@@ -16,11 +17,4 @@ function loopyLighthouse(){
   }
 }
 
-loopyLighthouse();
-
-// Write a program that prints the numbers from 100 to 200 to the console, with three exceptions:
-
-// If the number is a multiple of 3, print the string "Loopy" instead of the number.
-// If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
-// If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
-// By print, we are of course referring to console.log.
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
